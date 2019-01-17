b29e4f09a7877813fbf0d4970e82e1cdf3171dab
ultraji <ultraji@live.com>
2019-01-17 23:48:16 +0800

    文件夹结构调整

--- Linux-0.12/include/linux/config.h
+++ Linux-0.12/include/linux/config.h
@@ -6,15 +6,15 @@
  */
 #define UTS_SYSNAME "Linux"
 #define UTS_NODENAME "(none)"	/* set by sethostname() */
-#define UTS_RELEASE "0"		/* patchlevel */
+#define UTS_RELEASE "0"		    /* patchlevel */
 #define UTS_VERSION "0.12"
-#define UTS_MACHINE "i386"	/* hardware type */
+#define UTS_MACHINE "i386"	    /* hardware type */
 
 /* Don't touch these, unless you really know what your doing. */
-#define DEF_INITSEG	0x9000
-#define DEF_SYSSEG	0x1000
-#define DEF_SETUPSEG	0x9020
-#define DEF_SYSSIZE	0x3000
+#define DEF_INITSEG	0x9000      /* 该程序将要移动到的目的段位置 */
+#define DEF_SYSSEG	0x1000      /* 从磁盘加载系统模块到内存的段位置 */
+#define DEF_SETUPSEG	0x9020  /* setup程序代码的段位置 */
+#define DEF_SYSSIZE	0x3000      /* 系统模块长度 */
 
 /*
  * The root-device is no longer hard-coded. You can change the default
